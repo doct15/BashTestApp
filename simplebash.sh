045959095d632782934e1afd14a5e6317be844c7
doct15 <doc@tavian.com>
2015-05-21 15:24:50 -0700

    First

---
+++ simplebash.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+echo "Hello World from Distelli. You have deployed a simple Bash Script application."
+hosttype=`uname -s`
+nodename=`uname -n`
+arch=`uname -m`
+echo "The Server Name is:$nodename"
+echo "The Server Type is:$hosttype"
+echo "The Server Machine:$arch"
+echo ""
+echo "Your distelli-manifest.yml Environment Variable is:"
+echo "$EXAMPLE"
+echo ""
+for i in {1..10}
+do
+  date=`date`
+  echo "The Server Time is:$date"
+  sleep 8
+done
+  
+
+
+

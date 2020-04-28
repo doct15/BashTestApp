36dd04f43d219bb3f4314f900b83124641372c03
Brian McGehee <doct15@yahoo.com>
2020-04-28 00:29:24 -0700

    br2 test

--- test2.sh
+++ test2.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [ $(($(date +%s)%2)) -eq 1 ]; then
   echo "The number of seconds since 1/1/1970 is even"
+  echo "This was tested"
 else
   echo "The number of seconds since 1/1/1970 is odd"
+  echo "This was tested"
 fi
 

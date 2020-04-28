f779d87bb2f20b9409df5a081296e7129b296f64
Brian McGehee <doct15@yahoo.com>
2020-04-28 00:24:36 -0700

    pr 1

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 echo "Checking CODECOV_TOKEN exists"
-if [-z "$CODECOV_TOKEN" ]; then
+if [ -z "$CODECOV_TOKEN" ]; then
   echo "Please set CODECOV_TOKEN to your codecov.io Upload Token for this repo"
 fi
 echo "Building simplebash.sh"
@@ -8,7 +8,8 @@ echo "Creating directory for codecov"
 mkdir -p coverage
 echo "Running codecov"
 kcov coverage simplebash.sh
-kcov coverage script.sh test2.sh
+kcov coverage script.sh
+kcov coverage test2.sh
 echo "Sending report to codecov.io"
 bash <(curl -s https://codecov.io/bash)
 echo "Removing codecov report"
